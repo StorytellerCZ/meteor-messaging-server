b6c05b94d91a65e57f5b7c71f3d6f31508d4e9f1
StorytellerCZ <jxd3455@rit.edu>
2016-03-06 04:28:39 -0500

    :bug: Fix files to be properly on server

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+## v0.1.1 - 2016/3/06
+
+### New
+
+*   Added changelog
+
+### Fixes
+
+*   Properly set files to be on server.
+
+## v0.1.0 - 2016/3/05
+
+Initial move of server code into an independent package.
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'storyteller:messaging-server',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Server functionality for socialize:messaging',
   git: 'https://github.com/StorytellerCZ/meteor-messaging-server',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
   api.use('socialize:messaging@0.4.5')
-  api.addFiles('messaging-server.js');
+  api.addFiles(['messaging-server.js'], 'server');
 });
 
 Package.onTest(function(api) {
